23f71734eaa3d65d3ffa57e38d178d33b1214db6
Quang <dangminhquang7b.td@gmail.com>
2026-03-17 22:13:26 +0700

    done

--- backend/dist/config/database.d.ts
+++
@@ -1,5 +0,0 @@
-import sql, { config as SQLConfig } from 'mssql';
-export declare const dbConfig: SQLConfig;
-export declare const getConnection: () => Promise<sql.ConnectionPool>;
-export { sql };
-//# sourceMappingURL=database.d.ts.map
--- backend/dist/types/auth.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// ===========================
-// AUTH REQUEST DTOs
-// ===========================
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=auth.types.js.map
